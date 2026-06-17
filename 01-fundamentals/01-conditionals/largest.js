79b585c10bd73690344fe3114ad976c9d635a932
samiaUwase <samiaumut000@gmail.com>
2026-06-17 14:01:26 +0200

    add: largest.js

---
+++ 01-fundamentals/01-conditionals/largest.js
@@ -0,0 +1,9 @@
+let num1 =10;
+let num2 = 20;
+if(num1 > num2){
+    console.log(num1 + " is the largest number");
+}else if(num2 > num1){
+    console.log(num2 + " is the largest number");
+}else{
+    console.log("Both numbers are equal");
+}
