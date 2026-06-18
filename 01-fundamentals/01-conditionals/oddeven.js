8b39fcaac61641b671e03df7c22b9bf046ffe113
samiaUwase <samiaumut000@gmail.com>
2026-06-18 08:44:06 +0200

    oddeven.js

---
+++ 01-fundamentals/01-conditionals/oddeven.js
@@ -0,0 +1,16 @@
+let i;
+
+for(i=0;i<=15;i++){
+    if(i==0){
+        console.log(i+ ' is even');
+    }
+    else if(i%2==0)
+    {
+        console.log(i+ ' is even');
+
+    }
+
+    else{
+        console.log(i+ ' is odd');
+    }
+}
