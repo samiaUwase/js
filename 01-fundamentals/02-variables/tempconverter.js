7f5e6d6ac2547dc31d5dfe5282057baf7c5dd063
samiaUwase <samiaumut000@gmail.com>
2026-06-18 20:06:13 +0200

    Temperature converter

---
+++ 01-fundamentals/02-variables/tempconverter.js
@@ -0,0 +1,6 @@
+let celsius=30;
+let fahrenheit=(celsius*9/5)+32;
+console.log(celsius + '°C is ' + fahrenheit + '°F');
+fahrenheit=86;
+ celsius=(fahrenheit-32)*5/9;
+console.log(fahrenheit + '°F is ' + celsius + '°C');
