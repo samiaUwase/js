676dfa4a078baf3332aed4561e8c20053d02e73a
samiaUwase <samiaumut000@gmail.com>
2026-06-18 19:38:15 +0200

    lifetime supply calculator

---
+++ 01-fundamentals/02-variables/Lsupplycalculator.js
@@ -0,0 +1,6 @@
+let age = 28;
+let maxAge = 100;
+let numPerDay = 2;
+let totalNeeded = (numPerDay * 365) * (maxAge - age);
+
+console.log('You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge);
