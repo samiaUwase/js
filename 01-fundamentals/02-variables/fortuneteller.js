fe2a468534eba444d908a268ea7ffe6a0a864162
samiaUwase <samiaumut000@gmail.com>
2026-06-18 18:53:39 +0200

    storing values and strings in variables

---
+++ 01-fundamentals/02-variables/fortuneteller.js
@@ -0,0 +1,8 @@
+let numKids  = 5;
+let partner  = 'SOMEONE';
+let geoLocation = 'Costa Rica';
+let jobTitle = 'web developer';
+
+let future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' +
+   partner + ' ' + ' with ' + numKids + ' kids.';
+console.log(future);
